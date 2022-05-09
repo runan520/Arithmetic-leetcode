26db5d2d2edc3e690819723beebb22b0a46ce9fe
wuhepeng <1845664903@qq.com>
2022-05-09 14:18:33 +0800

    [31] NextPermutation

---
+++ src/main/java/leetcode/editor/en/MultiplyStrings.java
@@ -0,0 +1,41 @@
+package leetcode.editor.en;
+
+//Given two non-negative integers num1 and num2 represented as strings, return 
+//the product of num1 and num2, also represented as a string. 
+//
+// Note: You must not use any built-in BigInteger library or convert the inputs 
+//to integer directly. 
+//
+// 
+// Example 1: 
+// Input: num1 = "2", num2 = "3"
+//Output: "6"
+// Example 2: 
+// Input: num1 = "123", num2 = "456"
+//Output: "56088"
+// 
+// 
+// Constraints: 
+//
+// 
+// 1 <= num1.length, num2.length <= 200 
+// num1 and num2 consist of digits only. 
+// Both num1 and num2 do not contain any leading zero, except the number 0 
+//itself. 
+// 
+// Related Topics Math String Simulation 👍 4359 👎 1730
+
+public class MultiplyStrings{
+  public static void main(String[] args) {
+    Solution solution = new MultiplyStrings().new Solution();
+    
+  }
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public String multiply(String num1, String num2) {
+        return "";
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
